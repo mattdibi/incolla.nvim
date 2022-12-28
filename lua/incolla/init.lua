efaa1bdce5c55f14b84dcdf87d7e6b8ed528684f
Mattia Dal Ben <matthewdibi@gmail.com>
2022-12-28 12:54:51 +0100

    refactor: return table with content type and path

--- lua/incolla/init.lua
+++ lua/incolla/init.lua
@@ -23,7 +23,7 @@ local determine_clipboard_content = function()
     local reported_type = clip_info:match("[^,]+")
 
     if string.find(reported_type, "PNGf") or string.find(reported_type, "TIFF") then
-        return Content.IMAGE
+        return { Type = Content.IMAGE, Path = "" }
     elseif string.find(reported_type, "furl") then
         -- If clipboard type is file url, check it points to an actual image
         local clip_path_handle = io.popen('osascript -e "POSIX path of (the clipboard as «class furl»)"')
@@ -31,12 +31,12 @@ local determine_clipboard_content = function()
         clip_path_handle:close()
 
         if string.find(clip_path, ".png")  or string.find(clip_path, ".jpg") or string.find(clip_path, ".jpeg") then
-            return Content.FURL
+            return { Type = Content.FURL, Path = clip_path }
         else
-            return Content.UNSUPPORTED
+            return { Type = Content.UNSUPPORTED, Path = "" }
         end
     else
-        return Content.UNSUPPORTED
+        return { Type = Content.UNSUPPORTED, Path = "" }
     end
 end
 
@@ -49,7 +49,6 @@ local write_file = function(target_folder, file_name)
             ' (make new file at folder \\"' .. target_folder .. '\\"' ..
             ' with properties {name:\\"'.. file_name .. '\\"})"'
 
-    --io.popen(clip_command)
     os.execute(clip_command)
 end
 
@@ -73,13 +72,21 @@ M.incolla = function()
     local target_folder_full_path = current_folder .. "/" .. imgdir
     local target_folder_rel_path = "./" .. imgdir
 
-    local type = determine_clipboard_content()
+    local clip = determine_clipboard_content()
+    if clip.Type == Content.UNSUPPORTED then
+        -- Do nothing
+        return
+    end
 
     -- Create directory if missing
     create_dir(target_folder_full_path)
 
     -- Write new file to disk
-    write_file(target_folder_full_path, file_name)
+    if clip.Type == Content.IMAGE then
+        write_file(target_folder_full_path, file_name)
+    elseif clip.Type == Content.FURL then
+        -- Copy file to destination
+    end
 
     -- Add text at current position
     write_text(target_folder_rel_path, file_name)

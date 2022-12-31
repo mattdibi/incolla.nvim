3b4abfa56cb00d50bd8aec5a10eb868bff84295b
Mattia Dal Ben <matthewdibi@gmail.com>
2022-12-31 09:46:21 +0100

    refactor: simplify default options

--- lua/incolla/config.lua
+++ lua/incolla/config.lua
@@ -8,13 +8,7 @@ local DEFAULTS = {
     affix = "![](%s)",
 }
 
-M.options = {
-    img_dir = "imgs",
-    img_name = function()
-        return os.date("IMG-%d-%m-%Y-%H-%M-%S")
-    end,
-    affix = "![](%s)",
-}
+M.options = DEFAULTS
 
 M.set = function(opts)
     M.options.img_dir = opts.img_dir or DEFAULTS.img_dir

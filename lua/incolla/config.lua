480192a297617e9998812dc2573622eb49936159
Mattia Dal Ben <matthewdibi@gmail.com>
2022-12-31 16:15:22 +0100

    refactor: simplify config.set

--- lua/incolla/config.lua
+++ lua/incolla/config.lua
@@ -16,24 +16,13 @@ M.defaults = DEFAULTS
 --
 ---@param opts table: Table containing configuration options
 M.set = function(opts)
-    -- Defaults
-    M.defaults.img_dir = opts.defaults.img_dir or DEFAULTS.img_dir
-    M.defaults.img_name = opts.defaults.img_name or DEFAULTS.img_name
-    M.defaults.affix = opts.defaults.affix or DEFAULTS.affix
-
     -- Per filetype configuration
     for k, v in pairs(opts) do
-        if k == "defaults" then goto continue end
-
         M[k] = {
-            options = {
-                img_dir = v.options.img_dir or DEFAULTS.img_dir,
-                img_name = v.options.img_name or DEFAULTS.img_name,
-                affix = v.options.affix or DEFAULTS.affix
-            }
+            img_dir = v.img_dir or DEFAULTS.img_dir,
+            img_name = v.img_name or DEFAULTS.img_name,
+            affix = v.affix or DEFAULTS.affix
         }
-
-        ::continue::
     end
 end
 
@@ -44,7 +33,7 @@ end
 ---@param filetype string
 ---@return table
 M.get = function(filetype)
-    return M[filetype] ~= nil and M[filetype].options or M.defaults
+    return M[filetype] ~= nil and M[filetype] or M.defaults
 end
 
 return M

8e5b227bfdde1160445357da3e87bdc09216cec4
Mattia Dal Ben <matthewdibi@gmail.com>
2022-12-31 10:24:51 +0100

    docs: add docs for config module

--- lua/incolla/config.lua
+++ lua/incolla/config.lua
@@ -1,5 +1,6 @@
 local M = {}
 
+-- Default configuration options
 local DEFAULTS = {
     img_dir = "imgs",
     img_name = function()
@@ -8,8 +9,12 @@ local DEFAULTS = {
     affix = "![](%s)",
 }
 
+-- Configuration options table
 M.options = DEFAULTS
 
+-- Sets plugin configuration options
+--
+---@param opts table: Table containing configuration options
 M.set = function(opts)
     M.options.img_dir = opts.img_dir or DEFAULTS.img_dir
     M.options.img_name = opts.img_name or DEFAULTS.img_name
